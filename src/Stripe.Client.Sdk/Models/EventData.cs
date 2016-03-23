b0f24cb6f0e70082584299c682c5324d691468e9
jeff.repanich <jeff.repanich@thycotic.com>
2016-03-23 09:46:14 -0400

    parse event data object

--- src/Stripe.Client.Sdk/Models/EventData.cs
+++ src/Stripe.Client.Sdk/Models/EventData.cs
@@ -1,9 +1,63 @@
-﻿namespace Stripe.Client.Sdk.Models
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using Stripe.Client.Sdk.Helpers;
+
+namespace Stripe.Client.Sdk.Models
 {
     public class EventData
     {
-        public dynamic Object { get; set; }
+        private object _object;
+
+        public object Object
+        {
+            get { return _object; }
+            set
+            {
+                _object = Deserialize(value);
+            }
+        }
 
         public dynamic PreviousAttributes { get; set; }
+
+        private object Deserialize(object value)
+        {
+            var o = value as JObject;
+            if (o == null)
+            {
+                return null;
+            }
+            var key = o["object"].ToString();
+            if (string.IsNullOrWhiteSpace(key) || !_typeMap.ContainsKey(key))
+            {
+                return null;
+            }
+            var type = _typeMap[key];
+            return o.ToObject(type);
+        }
+
+        private readonly Dictionary<string, Type> _typeMap = new Dictionary<string, Type>
+        {
+            {"account", typeof (Account)},
+            {"application_fee", typeof (ApplicationFee)},
+            {"bank_account", typeof (BankAccount)},
+            {"bitcoin_reciever", typeof (BitcoinReceiver)},
+            {"card", typeof (Card)},
+            {"charge", typeof (Charge)},
+            {"coupon", typeof (Coupon)},
+            {"customer", typeof (Customer)},
+            {"discount", typeof (Discount)},
+            {"dispute", typeof (Dispute)},
+            {"fee_refund", typeof (ApplicationFeeRefund)},
+            {"invoice", typeof (Invoice)},
+            {"invoiceitem", typeof (InvoiceItem)},
+            {"order", typeof (Order)},
+            {"plan", typeof (Plan)},
+            {"product", typeof (Product)},
+            {"recipient", typeof (Recipient)},
+            {"sku", typeof (Sku)},
+            {"subscription", typeof (Subscription)},
+            {"transfer", typeof (Transfer)}
+        };
     }
 }
