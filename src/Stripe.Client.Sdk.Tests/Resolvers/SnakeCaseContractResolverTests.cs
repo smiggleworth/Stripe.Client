4b48d90b36a7d71f8b8caafcdc29ab3132a8e77b
jeff.repanich <jeff.repanich@thycotic.com>
2016-03-25 20:59:19 -0400

    resolvers test coverage

---
+++ src/Stripe.Client.Sdk.Tests/Resolvers/SnakeCaseContractResolverTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Stripe.Client.Sdk.Models;
+using Stripe.Client.Sdk.Resolvers;
+
+namespace Stripe.Client.Sdk.Tests.Resolvers
+{
+    [TestClass]
+    public class SnakeCaseContractResolverTests
+    {
+        [TestMethod]
+        public void ResolvePropertyName_ShouldSerialize()
+        {
+            // Arrange
+            var model = GenFu.GenFu.New<Sample>();
+
+            // Act
+            var json = JsonConvert.SerializeObject(model, new JsonSerializerSettings
+            {
+                ContractResolver = new SnakeCaseContractResolver()
+            });
+
+            // Assert
+            json.Should().Contain("some_prop")
+                .And.Contain("prop_with_number1")
+                .And.Contain("justprop");
+        }
+
+        [TestMethod]
+        public void ResolvePropertyName_ShouldDeserialize()
+        {
+            // Arrange
+            var json = "{\"some_prop\":\"Marsha!\",\"prop_with_number1\":\"Marsha!!\",\"justprop\":\"Marsha!!!\"}";
+
+            // Act
+            var model = JsonConvert.DeserializeObject<Sample>(json, new JsonSerializerSettings
+            {
+                ContractResolver = new SnakeCaseContractResolver()
+            });
+
+            // Assert
+            model.SomeProp.Should().Be("Marsha!");
+            model.PropWithNumber1.Should().Be("Marsha!!");
+            model.Justprop.Should().Be("Marsha!!!");
+        }
+
+        private class Sample
+        {
+            public string SomeProp { get; set; }
+            public string PropWithNumber1 { get; set; }
+            public string Justprop { get; set; }
+        }
+    }
+}
