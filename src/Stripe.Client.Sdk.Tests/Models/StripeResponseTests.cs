d4269905ba782a353237607c8e473e09b783af47
smiggleworth <jrepanich@hotmail.com>
2016-03-31 08:19:16 -0400

    test coverage for stripe response

---
+++ src/Stripe.Client.Sdk.Tests/Models/StripeResponseTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Stripe.Client.Sdk.Models;
+namespace Stripe.Client.Sdk.Tests.Models
+{
+    [TestClass]
+    public class StripeResponseTests
+    {
+        [TestMethod]
+        public void StripeResponse_Success()
+        {
+            var stripeResponse = new StripeResponse<string>();
+            stripeResponse.Success.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void StripeResponse_NotSuccess()
+        {
+            var stripeResponse = new StripeResponse<string>();
+            stripeResponse.Error = new StripeError();
+            stripeResponse.Success.Should().BeFalse();
+        }
+    }
+}
