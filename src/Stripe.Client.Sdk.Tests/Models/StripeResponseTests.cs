e71f1c7e11b52f90db158335fc292430aa6fd63c
jeff.repanich <jeff.repanich@thycotic.com>
2016-04-04 11:49:02 -0400

    remove junk

--- src/Stripe.Client.Sdk.Tests/Models/StripeResponseTests.cs
+++ src/Stripe.Client.Sdk.Tests/Models/StripeResponseTests.cs
@@ -3,6 +3,7 @@ using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 using Stripe.Client.Sdk.Models;
+
 namespace Stripe.Client.Sdk.Tests.Models
 {
     [TestClass]
@@ -21,24 +22,6 @@ namespace Stripe.Client.Sdk.Tests.Models
             var stripeResponse = new StripeResponse<string>();
             stripeResponse.Error = new StripeError();
             stripeResponse.Success.Should().BeFalse();
-        }
-    }
-
-    [TestClass]
-    public class StripeErrorEnvelopeTests
-    {
-        [TestMethod]
-        public void Event_Parsing()
-        {
-            // Arrange
-            var json = File.ReadAllText("JSON/error.json");
-
-            // Act
-            var obj = JsonConvert.DeserializeObject<Event>(json);
-
-            // Assert
-            obj.Should().BeAssignableTo<Event>();
-            obj.Data.Object.Should().BeAssignableTo<Invoice>();
         }
     }
 }
